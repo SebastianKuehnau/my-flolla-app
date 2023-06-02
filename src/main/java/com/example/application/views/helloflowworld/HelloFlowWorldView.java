a17d177c07dbd734329cf878e9b9c8dea77b680f
start.vaadin.com <no-reply@vaadin.com>
2023-06-02 10:48:46 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/helloflowworld/HelloFlowWorldView.java
@@ -0,0 +1,34 @@
+package com.example.application.views.helloflowworld;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Hello Flow World")
+@Route(value = "hello-flow-world")
+@RouteAlias(value = "")
+public class HelloFlowWorldView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public HelloFlowWorldView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+        sayHello.addClickShortcut(Key.ENTER);
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
